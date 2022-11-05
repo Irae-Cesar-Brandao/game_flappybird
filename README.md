1855740b4ee73e0c38510e02bdf6564eb3b543d3
Irae Cesar Brandao <73804334+Irae-Cesar-Brandao@users.noreply.github.com>
2022-11-05 09:45:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# flappybird
+ Jogo Flappybird Aulas de 2021 - Didático
+ Jogo elaborado durante os cursos de Programador WEB - FIC ano de 2021
+ Jogo utilizando linguagens de programação: html, css, javascript
+ A construção e elaboraçãodos jogos foi feita durante a realização de apredizados
+ tetes, aplicações, exercícios e demandas do curso realizao no IFTO 
+ trabalho individual sem fins comerciais somente exercicios de programação
+ 
---
+++ assets/js/flappy (normal).js
@@ -0,0 +1,235 @@
+/*Manipulando a arvore DOM  (conceitos de orientação a objeto)*/
+function novoElemento(tagName , className){
+    const elemento = document.createElement(tagName)
+    elemento.className = className
+    return elemento
+}
+
+/*Criação de um OBJETO com variaveis: com vários Valores - funções dentro de funções*/
+/*Criando componente BARREIRAS*/
+function Barreira(inferior = false){
+    this.elemento = novoElemento('div', 'barreira')
+    const corpo = novoElemento('div', 'corpo')
+    const borda = novoElemento('div', 'borda')
+    if(inferior){
+        this.elemento.appendChild(borda)
+        this.elemento.appendChild(corpo)
+    }else{
+        this.elemento.appendChild(corpo)
+        this.elemento.appendChild(borda)
+    }
+    this.setAltura = (altura) => corpo.style.height = altura+'px'
+}
+/* Criação dos pares de BARREIRAS*/
+function ParBarreiras(alturaGame,aberturaBarreiras,eixoX){
+    this.elemento = novoElemento('div', 'barreiras')
+    this.superior = new Barreira()
+    this.inferior = new Barreira(true)
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+/*Criando uma AERO FUNCTION dentro do js -> SET -COLOCAR VALOR EM X E GET - SABER VALOR DE X */
+    this.setX = (x) => this.elemento.style.left = x+'px'
+/*para saber se houve colisão do PASSARO com a BARREIRA pegar valor de X */
+    this.getX = () => {
+        px = this.elemento.style.left 
+        x = px.split('px')
+        return (parseInt(x))
+    }
+
+
+
+/* Criar uma Aero Function para  retornar o tamanho das barreiras pelo game no f12*/
+this.getWidht = () => this.elemento.clientWidth
+
+/*criar FUNÇÃO para sortear abertura das BARREIRAS*/
+    this.sortearAbertura = () => {
+        const alturaSuperior = parseInt(
+        Math.random() * (alturaGame - aberturaBarreiras))
+        const alturaInferior = alturaGame - alturaSuperior - aberturaBarreiras
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.setX(eixoX)
+/*chamada de sorteio de aberturas)*/
+    this.sortearAbertura()
+}
+
+/*Criando o GESTOR BARREIRAS*/
+function GestorBarreiras(alturaGame,larguraGame,aberturaBarreira,distanciaBarreira,deslocamento,pontos){
+    const nBarreiras = Math.round(larguraGame/distanciaBarreira)+1
+    this.barreiras = []
+/*calculo matemática colocar as barreiras no lugar delas*/
+    for(cont=0;cont<nBarreiras;cont++){
+        eixoX = larguraGame+distanciaBarreira*cont
+        this.barreiras.push(new ParBarreiras(alturaGame,aberturaBarreira,eixoX))
+    }
+    const meio= larguraGame/2
+    this.animar = () => {
+        this.barreiras.forEach((bar, indice) =>{
+            bar.setX(bar.getX()-deslocamento)
+            if(bar.getX()+bar.getWidht()<=0){
+                let posicao = (indice>0?indice:this.barreiras.length)
+                bar.setX(this.barreiras[posicao-1].getX()+distanciaBarreira)
+                bar.sortearAbertura()
+            }
+/* definindo o score de PONTUAÇÃO após passar o meio das barreiras*/
+            if(bar.getX()<meio && bar.getX()+deslocamento >=meio){
+                pontos()
+            }
+            })
+    }
+}
+
+/*Criando o PASSARO e suas características*/
+function Passaro(alturaGame){
+    this.elemento = novoElemento('img','passaro')
+    this.elemento.src = 'assets/images/passaro.png'
+/* LEMBRE= quem se movem são as BARREIRAS e não o passaro*/
+    this.getY = () =>{
+        px = this.elemento.style.bottom
+        y = px.split('px')
+        return (parseInt(y))
+    }
+    this.setY = (y) => this.elemento.style.bottom = y+'px'
+    this.setY(alturaGame/2)
+/*criando FUNÇÃO DE ANIMAÇÃO DO PASSARO*/
+this.voando = false
+/*let voando = false*/
+/*window.onkeydown = (e) => voando = true
+window.onkeyup = (e) => voando = false*/
+const alturaMaxima = alturaGame - this.elemento.clienteHeight
+this.animar = () =>{
+
+    let val = 0;
+    if(this.voando == 'subir'){
+        val = 5;
+    }else if(this.voando == 'decer'){
+        val = -5;
+    }
+    let novoy = this.getY()+val;
+    //let novoy = this.getY()+(this.voando ? 5:-5)
+        
+    
+    console.log(this.voando)
+    if(novoy<0){
+        novoy = 0
+    }else if(novoy>alturaMaxima)
+    {
+        novoy= alturaMaxima
+
+    }
+    this.setY(novoy)
+    }
+
+}   
+/*Criando PONTUAÇÃO*/
+function Pontuacao(){
+    this.elemento = novoElemento('span','pontos')
+    this.atualizarPontos = (pontos) => this.elemento.innerHTML = pontos
+    this.atualizarPontos(0)
+}
+/*
+function criarPlaca(){
+    const placa = {
+        pontuacao: 0,
+        desenha(){
+            contexto.font = '50px "pixel"';
+            contexto.textAlign = 'right';
+            contexto.fillStyle = 'white';
+            contexto.fillText (`Hello world ${placar.pontuacao}`, canvas.width - 10, 35);
+            placar.pontuacao
+        },
+        atualiza(){
+            const intervaloDeFrames = 10;
+            const passouOIntervalo = frames % intervaloDeFrames === 0;
+
+            if(passouOIntervalo){
+                placar.pontuacao = placar.pontuacao + 1;
+            }
+        }
+    }
+    return placar
+}
+*/
+/*criando a COLISÃO*/
+function colisao(elemento1,elemento2){
+    const a = elemento1.getBoundingClientRect()
+    const b = elemento2.getBoundingClientRect()
+
+    const colisaox = a.left + a.width >= b.left && b.left + b.width >= a.left
+    const colisaoy = a.top + a.height >= b.top && b.top + b.height >= a.top
+    return colisaox && colisaoy
+}
+function chekColisao(passaro,gestorbarreiras){
+    let colidiu = false
+    gestorbarreiras.barreiras.forEach((barreira) => {
+        const superior = barreira.superior.elemento
+        const inferior = barreira.inferior.elemento
+        if(colisao(passaro.elemento, superior)|| colisao(passaro.elemento, inferior)){
+            console.log("Você perdeu!")
+            colidiu = true
+        } 
+    })
+    return colidiu
+
+}
+/* criando  o FLAPPYBIRD */
+function flappyBird(){
+    const game = document.querySelector('.game')
+/*Definir a ALTURA e a LARGURA do GAME*/
+    const altura = game.clientHeight
+    const largura = game.clientWidth
+    
+/*Recriando  código do PASSARO- iniciando JavaScript barreira DOM*/
+    const passaro = new Passaro(altura)
+    const pontuacao = new Pontuacao()
+    let pontos = 0
+    const gestorBarreiras = new GestorBarreiras(altura,largura,200,400,4,() =>pontuacao.atualizarPontos(++pontos))
+    game.appendChild(passaro.elemento)
+    gestorBarreiras.barreiras.forEach(barreira => {
+        game.appendChild(barreira.elemento)
+})
+
+/*função de iniciar o jogo e criando variável temporizador*/
+    game.appendChild(pontuacao.elemento)
+    this.start = () => {
+        const temporizador = setInterval(() =>{
+            gestorBarreiras.animar()
+            passaro.animar()
+            if(chekColisao(passaro, gestorBarreiras)){
+                clearInterval(temporizador)
+                if(confirm("Você perdeu com um total de "+pontos+"pontos. Gostaria de jogar novamente?")){
+                    document.location.reload()
+                }
+            }
+        },40)
+
+    }  
+     window.onkeydown = (e) =>{
+        
+
+        passaro.voando = "reto"
+        if(e.keyCode == 38){
+            passaro.voando = 'subir'
+        }else if(e.keyCode == 40){
+            passaro.voando = 'decer'
+        }
+       /*p=80 up=38  down=40*/
+
+     } 
+     window.onkeyup = (e) => {
+        passaro.voando = false
+
+     }  
+}
+/*comando de START do JOGO*/
+new flappyBird().start()
+
+
+
+
+
+
+
+
